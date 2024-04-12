56c470e2cd6c1cbbd30e127d35215e62758a25cd
Juwon <juwon5196@gmail.com>
2024-04-12 17:48:52 +0900

    [주원] 스프링 부트 프로젝트 생성 실습

---
+++ HelloWOrld/src/main/java/com/bbanggood/helloworld/HelloWOrldApplication.java
@@ -0,0 +1,13 @@
+package com.bbanggood.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWOrldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWOrldApplication.class, args);
+	}
+
+}
