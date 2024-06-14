fdfb1b1fa0cfd87b3e57bb974ca81184ca7130a2
Hwan <76547975+wjdghks9292@users.noreply.github.com>
2024-06-14 17:46:19 +0900

    Delete springrecommend directory

--- springrecommend/src/main/java/com/bbanggood/springrecommend/repository/RecommendRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bbanggood.springrecommend.repository;
-
-import com.bbanggood.springrecommend.entity.RecommendEntity;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RecommendRepository extends MongoRepository<RecommendEntity, ObjectId> {
-    RecommendEntity findBySetbxId(String setbxId);
-}
