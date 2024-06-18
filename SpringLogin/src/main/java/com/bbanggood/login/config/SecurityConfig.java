3f94e7bf3b41a7ac0a5391c47c1df33afc64c760
wjdghks9494 <wjdghks9292@naver.com>
2024-06-18 15:04:34 +0900

    [정환] 로그인 프로젝트 수정

--- SpringLogin/src/main/java/com/bbanggood/login/config/SecurityConfig.java
+++ SpringLogin/src/main/java/com/bbanggood/login/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
                         configuration.setMaxAge(3600L);
 
                         configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+                        configuration.setExposedHeaders(Collections.singletonList("x-amzn-Remapped-Authorization"));
 
                         return configuration;
                     }
