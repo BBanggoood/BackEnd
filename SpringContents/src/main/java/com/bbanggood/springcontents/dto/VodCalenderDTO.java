9916d669b906da90dd02325d0d95893ba932dcd0
Juwon <juwon5196@gmail.com>
2024-06-18 17:28:35 +0900

    [주원] 캘린더 vod 연결

--- SpringContents/src/main/java/com/bbanggood/springcontents/dto/VodCalenderDTO.java
+++ SpringContents/src/main/java/com/bbanggood/springcontents/dto/VodCalenderDTO.java
@@ -2,12 +2,14 @@ package com.bbanggood.springcontents.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.bson.types.ObjectId;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 public class VodCalenderDTO {
+    private String vodId;
     private String vodName;
     private Date vodOpenAt;
     private String vodDirector;
